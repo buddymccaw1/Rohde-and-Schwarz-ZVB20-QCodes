be3a707ca3473a4c4777a116bdc1c65b30cc357b
buddymccaw <134028213+buddymccaw1@users.noreply.github.com>
2023-05-19 14:03:34 +1000

    Add files via upload

---
+++ rohde_schwarz/Rohde_Schwarz_ZVB20.py
@@ -0,0 +1,10 @@
+from .ZNB import ZNB
+
+
+class RohdeSchwarzZVB20(ZNB):
+    """
+    QCoDeS driver for Rohde & Schwarz ZVB20
+    
+    """
+    
+    pass
---
+++ rohde_schwarz/ZVB20.py
@@ -0,0 +1,5 @@
+# Ensuring backwards compatibility
+
+from .ZNB import ZNB
+
+ZVB20 = ZNB
